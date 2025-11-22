3179755fe8f99444c28732edec96944209f46b15
Steven Noack <MacStenk@users.noreply.github.com>
2025-11-22 08:28:00 +0100

    Remove TinaCMS and add Tailwind Vite integration
    Removed all TinaCMS-related files and dependencies, including generated files and config. Added @tailwindcss/vite to dependencies and configured it in astro.config.mjs. Also deleted several unused components and legal pages, and added a new 'rechtliches.astro' page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://stevennoack.de',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/stevennoack/dev/stevennoack.de/tina/__generated__/.cache/1762801907196', url: 'https://content.tinajs.io/1.6/content/04361101-fd41-451a-9582-8ea748c53ccd/github/main', token: 'df66d8e493aa21228ac14c90c3005bcef07f0819', queries,  });
-export default client;
-  
